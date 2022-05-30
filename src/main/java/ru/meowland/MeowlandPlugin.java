26a0110322984f98732ec56583204c2ae5a071e5
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-05-30 20:51:10 +0300

    add spawn command

--- src/main/java/ru/meowland/MeowlandPlugin.java
+++ src/main/java/ru/meowland/MeowlandPlugin.java
@@ -2,12 +2,14 @@ package ru.meowland;
 
 import arc.Events;
 import arc.util.CommandHandler;
+import mindustry.Vars;
 import mindustry.game.EventType;
-import mindustry.gen.Call;
-import mindustry.gen.Groups;
-import mindustry.gen.Player;
-import mindustry.gen.Unitc;
+import mindustry.game.Team;
+import mindustry.gen.*;
 import mindustry.mod.Plugin;
+import mindustry.net.Administration;
+import mindustry.net.Administration.Config;
+import mindustry.type.UnitType;
 
 public class MeowlandPlugin extends Plugin {
     @Override
@@ -16,6 +18,7 @@ public class MeowlandPlugin extends Plugin {
             Player player = event.player;
             Call.sendMessage("[lime]Игрок [#B](" + player.name + "[#B]) [lime]зашёл");
         });
+
     }
     @Override
     public void registerClientCommands(CommandHandler handler){
@@ -35,6 +38,55 @@ public class MeowlandPlugin extends Plugin {
                 return;
             }
 
+        });
+        handler.<Player>register("spawn", "<Unit> <Count> <Team>", "Спавнит нужного юнита", (args, player) ->{
+            if(player.admin){
+                UnitType unit = Vars.content.units().find(b -> b.name.equals(args[0]));
+                int count;
+                try {
+                    count = Integer.parseInt(args[1]);
+                }catch (NumberFormatException e){
+                    player.sendMessage("[red]Число должно быть числом");
+                    return;
+                }
+                player.sendMessage("0: " + args[0] + "1: " + args[1] + "2: " + args[2]);
+                Team team;
+                if (args[2].equals("sharded")) {
+                    team = Team.sharded;
+                } else if(args[2].equals("blue")){
+                    team = Team.blue;
+                } else if(args[2].equals("crux")){
+                    team = Team.crux;
+                } else if (args[2].equals("derelict")) {
+                    team = Team.derelict;
+                } else if (args[2].equals("green")) {
+                    team = Team.green;
+                } else if(args[2].equals("purple")){
+                    team = Team.purple;
+                } else {
+                    player.sendMessage("Есть команды: [yellow]sharded[], [blue]blue[], [red]crux[], [gray]derelict[], [green]green[], [purple]purple[].");
+                    player.sendMessage(args[2]);
+                    return;
+                }
+                if(count > 15){
+                    player.sendMessage("Лимит для спавна 15 юнитов");
+                    return;
+                }
+                if(unit != null){
+                    for (int i = 0; count > i; i++) {
+                        Unit unit1 = unit.spawn(team, player.x, player.y);
+                    }
+                    player.sendMessage("[green]Сексесфул");
+                }else {
+                    player.sendMessage("Есть юниты: [accent]dagger, mace, fortress, scepter, reign, nova, pulsar, quasar, vela, corvus, crawler, atrax, spiroct, arkyid, toxopid, mono, poly, mega, quad, oct, flare, eclipse, horizon, zenith, antumbra, risso, minke, bryde, sei, omura");
+                }
+
+
+            }
+            else {
+                player.sendMessage("[red]Ты не админ");
+                return;
+            }
         });
     }
 }
