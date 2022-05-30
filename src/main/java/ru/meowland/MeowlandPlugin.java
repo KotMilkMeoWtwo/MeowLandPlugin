956e0f7b4050203fd48e028bf0bd568b11bdae22
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-05-30 19:35:06 +0300

    Добавлина команда шизы и сообщение о входа

--- src/main/java/ru/meowland/MeowlandPlugin.java
+++ src/main/java/ru/meowland/MeowlandPlugin.java
@@ -1,8 +1,24 @@
 package ru.meowland;
 
+import arc.Events;
 import arc.util.CommandHandler;
+import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 
 public class MeowlandPlugin extends Plugin {
-
+    @Override
+    public void init(){
+        Events.on(EventType.PlayerConnect.class, event ->{
+            Player player = event.player;
+            Call.sendMessage("[lime]Игрок [#B](" + player.name + "[#B]) [lime]зашёл");
+        });
+    }
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register("meow", "<text...>", "позволяет разговаривать с самим собой. никто, кроме консоли, не узанет, что ты тут пишешь",  (args, player) -> {
+            player.sendMessage(args[0]);
+        });
+    }
 }
