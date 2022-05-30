4b90110c3c1cc6bff80e9cf189780819a9f88861
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-05-30 19:51:50 +0300

    add command

--- src/main/java/ru/meowland/MeowlandPlugin.java
+++ src/main/java/ru/meowland/MeowlandPlugin.java
@@ -4,7 +4,9 @@ import arc.Events;
 import arc.util.CommandHandler;
 import mindustry.game.EventType;
 import mindustry.gen.Call;
+import mindustry.gen.Groups;
 import mindustry.gen.Player;
+import mindustry.gen.Unitc;
 import mindustry.mod.Plugin;
 
 public class MeowlandPlugin extends Plugin {
@@ -17,8 +19,22 @@ public class MeowlandPlugin extends Plugin {
     }
     @Override
     public void registerClientCommands(CommandHandler handler){
-        handler.<Player>register("meow", "<text...>", "позволяет разговаривать с самим собой. никто, кроме консоли, не узанет, что ты тут пишешь",  (args, player) -> {
+        handler.<Player>register("shiza",
+                "<text...>",
+                "позволяет разговаривать с самим собой. никто, кроме консоли, не узанет, что ты тут пишешь",  (args, player) -> {
             player.sendMessage(args[0]);
+        });
+
+        handler.<Player>register("despw", "", "Убивает всех юнитов на сервере", (args, player) -> {
+            if(player.admin){
+                Groups.unit.each(Unitc::kill);
+                player.sendMessage("Все юниты померли");
+            }
+            else {
+                player.sendMessage("[red]Ты не админ");
+                return;
+            }
+
         });
     }
 }
