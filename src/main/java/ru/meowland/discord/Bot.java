5fa6a36e522080ba78342c26ff6ed753dbf29526
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-06-10 18:29:45 +0300

    обновочка

--- src/main/java/ru/meowland/discord/Bot.java
+++ src/main/java/ru/meowland/discord/Bot.java
@@ -3,15 +3,19 @@ package ru.meowland.discord;
 import arc.Events;
 import arc.struct.ObjectMap;
 import arc.util.Log;
+import mindustry.Vars;
 import mindustry.game.EventType;
+import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
+import ru.meowland.config.Bundle;
 import ru.meowland.config.Config;
 
 import javax.security.auth.login.LoginException;
+import java.time.temporal.TemporalAccessor;
 
 public class Bot {
     public void bot() throws LoginException {
@@ -24,8 +28,25 @@ public class Bot {
             Events.on(EventType.PlayerConnect.class, event ->{
                 Player player = event.player;
                 GuildMessageChannel ch = jda.getChannelById(GuildMessageChannel.class, Config.get("channel_id"));
-                ch.sendMessage("meow").queue();
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle(Config.get("server_name"));
+                eb.addField(player.name, Bundle.get("discord.join"),  false);
+                eb.addField(Bundle.get("discord.count"), String.valueOf(Groups.player.size()+1), false);
+                eb.setColor(3211008);
 
+                //ch.sendMessage("meow").queue();
+                ch.sendMessageEmbeds(eb.build()).queue();
+            });
+            Events.on(EventType.PlayerLeave.class, event -> {
+                Player player = event.player;
+                GuildMessageChannel ch = jda.getChannelById(GuildMessageChannel.class, Config.get("channel_id"));
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle(Config.get("server_name"));
+                eb.addField(player.name, Bundle.get("discord.leave"), false);
+                eb.setColor(9109504);
+                eb.addField(Bundle.get("discord.count"), String.valueOf(Groups.player.size()-1), false);
+                //ch.sendMessage("meow").queue();
+                ch.sendMessageEmbeds(eb.build()).queue();
             });
         }
     }
